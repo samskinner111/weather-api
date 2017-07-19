7d3c1bbf7b471864bb9c1b3669f0812df01caa5f
samskinnerphd <samskinnerphd@gmail.com>
2017-07-19 14:29:46 -0400

    configure test view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,15 @@
 class WelcomeController < ApplicationController
   def test
+    response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/AZ/Phoenix.json")
+  
+    @location = response['location']['city']
+    @temp_f = response['current_observation']['temp_f']
+    @temp_c = response['current_observation']['temp_c']
+    @weather_icon = response['current_observation']['icon_url']
+    @weather_words = response['current_observation']['weather'] 
+    @forecast_link = response['current_observation']['forecast_url']
+    @real_feel = response['current_observation']['feelslike_f']
+    @wind_direction = response['current_observation']['wind_dir']
   end
 
   def index
