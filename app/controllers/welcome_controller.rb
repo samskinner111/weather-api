fdeeb2bfa630a2d903b243457f5b8b4652c40c9d
samskinnerphd <samskinnerphd@gmail.com>
2017-07-24 20:02:03 -0400

    remove duplicate index method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,40 +12,6 @@ class WelcomeController < ApplicationController
     @wind_direction = response['current_observation']['wind_dir']
   end
 
-  # def index
-  #   # Creates an array of states that our user can choose from on our index page
-  #   @states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK 
-  #    TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY 
-  #    NJ CT RI MA VT NH ME DC).sort!
-
-  #  @locations = Location.order(:city)  
-
-  #   # removes spaces from the 2-word city names and replaces the space with an underscore 
-  #   if params[:city] != nil
-  #     params[:city].gsub!(" ", "_")
-  #   end
-
-  #   #checks that the state and city params are not empty before calling the API
-  #   if params[:state] != "" && params[:city] != "" && params[:state] != nil && params[:city] != nil
-  #     results = HTTParty.get("http://api.wunderground.com/api/#{Figaro.env.wunderground_api_key}/geolookup/conditions/q/#{params[:state]}/#{params[:city]}.json")
-      
-  #     # if no error is returned from the call, we fill our instants variables with the result of the call  
-  #     if results['response']['error'] == nil || results['error'] ==""   
-  #       @location = results['location']['city']
-  #       @temp_f = results['current_observation']['temp_f']
-  #       @temp_c = results['current_observation']['temp_c']
-  #             @weather_icon = results['current_observation']['icon_url']
-  #       @weather_words = results['current_observation']['weather']
-  #             @forecast_link = results['current_observation']['forecast_url']
-  #       @real_feel_f = results['current_observation']['feelslike_f']
-  #       @real_feel_c = results['current_observation']['feelslike_c']
-  #     else
-  #     # if there is an error, we report it to our user via the @error variable  
-  #     @error = results['response']['error']['description']
-  #     end 
-  #   end
-  # end
-
   def index
     # Creates an array of states that our user can choose from on our index page
     @states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC).sort!
@@ -74,8 +40,7 @@ class WelcomeController < ApplicationController
      
       results = HTTParty.get("http://api.wunderground.com/api/#{Figaro.env.wunderground_api_key}/geolookup/conditions/q/#{params[:state]}/#{params[:city]}.json")
       
-    # if no error is returned from the call, we fill our instants variables with the result of the call
-     
+    # if no error is returned from the call, we fill our instants variables with the result of the call  
       if results['response']['error'] == nil || results['error'] == ""    
         @city = results['location']['city']
         @state = results['location']['state']
@@ -87,7 +52,6 @@ class WelcomeController < ApplicationController
         @real_feel_f = results['current_observation']['feelslike_f']
         @real_feel_c = results['current_observation']['feelslike_c']
       else
-
         # if there is an error, we report it to our user via the @error variable  
         @error = results['response']['error']['description']
       end
